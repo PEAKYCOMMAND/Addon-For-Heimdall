1214c6d77cdb6afeaca6f217a523c8034c074b0c
PEAKY? <91558914+PEAKYCOMMAND@users.noreply.github.com>
2022-09-10 11:45:02 -0300

    Add files via upload

---
+++ program.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+CK='\e[30m'                                                                 RED='\e[31m'                                                                GREEN='\e[32m'                                                              YELLOW='\e[33m'                                                             BLUE='\e[34m'                                                               PURPLE='\e[35m'                                                             CYAN='\e[36m'                                                               WHITE='\e[37m'                                                              NC='\e[0m'
+
+echo -e "${GREEN} =============================================================                     ${NC}"
+                                                                             echo -e  "${GREEN}
+            ╭━━━╮╱╭╮╱╭╮╱╱╱╱╱╱╱╭━╮╱╱╱╱╱╭╮╱╭╮╱╱╱╱╱╱╱╱╭╮╱╱╭╮╭╮
+            ┃╭━╮┃╱┃┃╱┃┃╱╱╱╱╱╱╱┃╭╯╱╱╱╱╱┃┃╱┃┃╱╱╱╱╱╱╱╱┃┃╱╱┃┃┃┃                             ┃┃╱┃┣━╯┣━╯┣━━┳━╮╱╭╯╰┳━━┳━╮┃╰━╯┣━━┳┳╮╭┳━╯┣━━┫┃┃┃                             ┃╰━╯┃╭╮┃╭╮┃╭╮┃╭╮╮╰╮╭┫╭╮┃╭╯┃╭━╮┃┃━╋┫╰╯┃╭╮┃╭╮┃┃┃┃                             ┃╭━╮┃╰╯┃╰╯┃╰╯┃┃┃┃╱┃┃┃╰╯┃┃╱┃┃╱┃┃┃━┫┃┃┃┃╰╯┃╭╮┃╰┫╰╮                            ╰╯╱╰┻━━┻━━┻━━┻╯╰╯╱╰╯╰━━┻╯╱╰╯╱╰┻━━┻┻┻┻┻━━┻╯╰┻━┻━╯                ${NC}"                                                                      
+                                                                             echo -e "${YELLOW}
+ |Author  : PEAKYCOMMAND
+ |Contact : https://t.me/PEAKY778G                                           |Version : 2.0
+ |Credits to JonesqPacMan for the Heimdall module                           ${NC}"                                                                      
+  echo -e "${GREEN} ===========================================================   
+
+
+  ${NC}"
+   echo -e "${YELLOW} Move the files you want to flash to /Download/heimdall/ folder then run heimdall normally.
+"${NC}
+   echo -e "${YELLOW} for more information run  'heimdall'.
+
+type 'exit' to exit.
+${NC}"
+
+cd storage/downloads/heimdall
+   echo -e  "${GREEN} Entering the folder with root /downloads/heimdall/${NC}"
+sleep 3
+su
